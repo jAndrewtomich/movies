60c3f0600dc91505a180f00e55b34e97fe255094
jatomic <johnatomich@gmail.com>
2022-01-16 22:42:21 -0600

    Create User and Profile classes

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    profiles = models.ManyToManyField('Profile', null=True, blank=True)
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=225)
+    age_limit = models.CharField(max_length=10, choices=AGE_CHOICES)
